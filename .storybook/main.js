b68c06fb258f25f986a683aeb226a3d4aeef7272
Roy Lopez <roylopezdev@gmail.com>
2023-11-06 16:02:13 -0300

    Add first Story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -13,6 +14,16 @@ const config = {
   },
   docs: {
     autodocs: "tag",
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+      propFilter: () => true,
+    },
   },
 };
 export default config;
